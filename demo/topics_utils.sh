2ca6cb00a3cd06a0c833fd9a64d723ab67c19b0b
Balint Bene <balintdbene@gmail.com>
2020-01-06 13:34:15 -0500

    Set retention time on topics in bytes

--- demo/topics_utils.sh
+++ demo/topics_utils.sh
@@ -2,14 +2,17 @@
 
 source util.sh
 
-KAFKA_CREATE_CMD='kafka-topics --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic'
-KAFKA_LIST_TOPIC_CMD='kafka-topics --list --bootstrap-server localhost:9092'
+CONTAINER_NAME='broker'
+BOOTSTRAP_SERVER='localhost:9092'
+KAFKA_CREATE_CMD="kafka-topics --create --bootstrap-server ${BOOTSTRAP_SERVER} --replication-factor 1 --partitions 1 --config retention.bytes=10485760 --topic"
+KAFKA_LIST_TOPIC_CMD="kafka-topics --list --bootstrap-server ${BOOTSTRAP_SERVER}"
+KAFKA_DESCRIBE_TOPIC_CMD="kafka-topics --describe --bootstrap-server ${BOOTSTRAP_SERVER} --topic"
 DOCKER_CMD='docker exec'
 ZK_SHELL_CMD='zookeeper-shell localhost:2181'
 
 create_topic() {
     TOPIC_NAME=${1}
-    ${DOCKER_CMD} 'broker' ${KAFKA_CREATE_CMD} ${TOPIC_NAME}
+    ${DOCKER_CMD} ${CONTAINER_NAME} ${KAFKA_CREATE_CMD} ${TOPIC_NAME}
 
     if [ $? -eq 0 ]; then
         log_ok "Topic created: ${TOPIC_NAME}"
@@ -20,5 +23,18 @@ create_topic() {
 
 list_topics() {
     log_info "The following topics exist:"
-    ${DOCKER_CMD} 'broker' ${KAFKA_LIST_TOPIC_CMD}
+    ${DOCKER_CMD} ${CONTAINER_NAME} ${KAFKA_LIST_TOPIC_CMD}
+}
+
+describe_topic() {
+    TOPIC_NAME=${1}
+    log_info "Description of topic $TOPIC_NAME:"
+    ${DOCKER_CMD} ${CONTAINER_NAME} ${KAFKA_DESCRIBE_TOPIC_CMD} ${TOPIC_NAME}
+}
+
+alter_topic() {
+    TOPIC_NAME=${1}
+    CONFIG=${2}
+
+
 }
